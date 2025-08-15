d9018a7e7072e7cb914f4a1be9e417bed21d3398
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2025-08-15 00:25:53 -0500

    impl any toolchain variants

--- variants/any-ios-any-nonfree-static.sh
+++ variants/any-ios-any-nonfree-static.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source "$(dirname "$BASH_SOURCE")"/linux64-gpl.sh
+source "$(dirname "$BASH_SOURCE")"/any-macos-any-gpl-static.sh
 FF_CONFIGURE="--enable-nonfree $FF_CONFIGURE"
 LICENSE_FILE=""
